e5679d67eda3ec1c635bb027afe287b198dcc6fd
optimeio <theoptime.io@gmail.com>
2026-09-04 18:55:43 +0530

    feat: add backend/.env.example template and db.js DNS fallback

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,17 @@
 import mongoose from 'mongoose';
 import dns from 'dns';
 
+try {
+  dns.setServers(['8.8.8.8', '1.1.1.1']);
+} catch (e) {}
+
 if (process.platform === 'win32') {
   try {
     dns.setDefaultResultOrder('ipv4first');
   } catch (e) {}
 }
 
-const DIRECT_FALLBACK_URI = "mongodb://mbkdrive82_db_user:K21TjnzWV7zdn3Bj@ac-n61j1cy-shard-00-00.qq9k2vl.mongodb.net:27017,ac-n61j1cy-shard-00-01.qq9k2vl.mongodb.net:27017,ac-n61j1cy-shard-00-02.qq9k2vl.mongodb.net:27017/id_scan?ssl=true&replicaSet=atlas-iqb4s7-shard-0&authSource=admin&retryWrites=true&w=majority";
+const DIRECT_FALLBACK_URI = "mongodb://mbkattendancedrive_db_user:s2jkJ5f2p6GsCPqc@ac-0xbnm87-shard-00-00.rj7k6f6.mongodb.net:27017,ac-0xbnm87-shard-00-01.rj7k6f6.mongodb.net:27017,ac-0xbnm87-shard-00-02.rj7k6f6.mongodb.net:27017/id_scan?ssl=true&replicaSet=atlas-ve97o4-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 export const connectDB = async () => {
   const uri = process.env.MONGODB_URI || DIRECT_FALLBACK_URI;
